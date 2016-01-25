3692d56a7f61e297abc892c8f9f34f107f917d87
Jared Sulzdorf <jared@lexblog.com>
2016-01-25 12:44:14 -0800

    finished widget boilerplate

--- inc/widget.php
+++ inc/widget.php
@@ -21,61 +21,63 @@ class Unique_Widget_Name extends WP_Widget {
 	}
 
 	//build the form
-	function form(  ) {
+	function form( $instance ) {
 
-		//first set up the default values in case  doesn't have all of them
-		 = array();
-		 = wp_parse_args( ( array ) ,  );
+		//first set up the default values in case $instance doesn't have all of them
+		$defaults = array();
+		$instance = wp_parse_args( ( array ) $instance, $defaults );
 
 		//next set the values from the instance object to be unique variables so that they can be used in the form
 		//here's an example of how to do this for the title
 
 		//first set up a blank value for the variables that our defaults don't catch
-		 = '';
-		//next, check to see if those values are actually set in 
-		if ( isset( ['title'] ) ) {
-			//if  has a value for that key, then set the blank variable to equal that value!
-			 = ['title'];
+		$title = '';
+		//next, check to see if those values are actually set in $instance
+		if ( isset( $instance['title'] ) ) {
+			//if $instance has a value for that key, then set the blank variable to equal that value!
+			$title = $instance['title'];
 		}
 
 		//then use that variable in the form
 		?>
 
 			<p>Title: 
-				<input class='widefat' name="<?php echo ->get_field_name( 'title' ); ?>" type='text' value="<?php echo esc_attr(  ); ?>" />
+				<input class='widefat' name="<?php echo $this->get_field_name( 'title' ); ?>" type='text' value="<?php echo esc_attr( $title ); ?>" />
 			</p>
 
 		<?php
 	}
 
 	//save the widget settings
-	function update( ,  ) {
+	function update( $new_instance, $old_instance ) {
 
-		//set the  object equal to an array
-		 = array();
-		//set up the  by checking the value of the new_instance to make sure the values passed in the form 
-		['title'] = ( ! empty( ['title'] ) ) ? strip_tags( ['title'] ) : '';
+		//set the $instance object equal to an array
+		$instance = array();
+		//set up the $instance by checking the value of the new_instance to make sure the values passed in the form 
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
 
-		return ;
+		return $instance;
 
 	}
 
 	//output the widget
-	function widget( ,  ) {
+	function widget( $args, $instance ) {
 
-		//this gives us access to some default arguments that we can use to display standard WordPress information like  
-		extract();
+		//this gives us access to some default arguments that we can use to display standard WordPress information like $before_widget 
+		extract($args);
 
-		echo ;
+		echo $before_widget;
 		//output all of the information
-		 = apply_filters( 'widget_title', ['title'] );
+		$title = apply_filters( 'widget_title', $instance['title'] );
 
-		echo  .  . ;
+		echo $before_title . $title . $after_title;
 
 
 
 	}
 
 }
+
+?>
 
 ?>
