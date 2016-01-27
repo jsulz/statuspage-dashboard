79d54c2f7d74149958a9c739cc64c0c685f8661f
Jared Sulzdorf <jared@lexblog.com>
2016-01-26 22:05:48 -0800

    added main plugin file

---
+++ statuspage-dashboard.php
@@ -0,0 +1,90 @@
+<?php
+/*
+	Plugin Name: Status Page Dashboard
+	Plugin URI: https://profiles.wordpress.org/jsulz
+	Description: A plugin that can show your application's/company's Status Page components and information in a variety of fashions
+	Author: Jared Sulzdorf
+	Version: 0.1-alpha
+	Author URI: https://profiles.wordpress.org/jsulz
+ */
+	
+// Peace out if you're trying to access this up front
+if( ! defined( 'ABSPATH' ) ) exit;
+
+//If this class don't exist, make it so
+if( ! class_exists( 'STATDASH' ) ) {
+
+	class STATDASH {
+
+		private static $instance;
+
+			//the magic
+	        public static function instance() {
+
+	            if( ! self::$instance ) {
+
+	                self::$instance = new STATDASH( );
+	                self::$instance->plugin_constants( );
+	                self::$instance->plugin_requires( );
+	                add_action( 'wp_enqueue_scripts', array( self::$instance, 'load_all_scripts' ) );
+	                //check when text domain can be queued up and load appropriately
+
+	            }
+
+	            return self::$instance;
+
+	        }
+
+	    //the constants (folders and such)
+		public function plugin_constants() {
+
+			define( 'STATDASH_FOLDER', plugin_dir_path( __FILE__ ) );
+			define( 'STATDASH_INC', trailingslashit( STATDASH_FOLDER . 'inc' ) );
+			define( 'STATDASH_CSS', trailingslashit( STATDASH_FOLDER . 'css' ) );
+			define( 'STATDASH_JS', trailingslashit( STATDASH_FOLDER . 'js' ) );	
+			define( 'STATDASH_SHORTCODES', STATDASH_INC . 'shortcodes.php');
+			define( 'STATDASH_WIDGET', STATDASH_INC . 'widget.php' );
+			define( 'STATDASH_API_CLIENT', STATDASH_INC . 'client.php' );
+			define( 'CSS_VERSION', '0.1' );
+
+		}
+
+		//the files
+		public function plugin_requires() {
+
+			require( STATDASH_SHORTCODES );
+			require( PLUGIN_SCRIPTS ) ;
+			require( STATDASH_WIDGET );
+			require( STATDASH_API_CLIENT );
+
+		}
+		//in case someone wants to translate stuff 
+		//Need to refactor as I might need to load this differently similar to load_all_scripts()
+		public function class_name_load_plugin_textdomain() {
+
+	    	load_plugin_textdomain( 'text-domain', FALSE, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+		}
+
+		//Add this to it's own file - scripts.php
+		//Don't hardcode version number - add as constant
+		public function load_all_scripts() {
+
+			wp_enqueue_style( 'handle', plugin_dir_url( __FILE__ ) . 'css/styles.css', array(), CSS_VERSION, 'all' );
+		}
+		
+	}
+
+}
+
+//get this show on the road
+function statdash_load() {
+
+    return STATDASH::instance( );
+    
+}
+
+//Check to see if this can be done differently 
+add_action( 'plugins_loaded', 'statdash_load' );
+
+?>
